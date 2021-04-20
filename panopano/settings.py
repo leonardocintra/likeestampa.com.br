5825aac7c1534e1ce1f2d5b4e4f3221663a952b5
leo <leonardo.ncintra@outlook.com>
2021-04-20 19:54:27 -0300

    alowed host

--- panopano/settings.py
+++ panopano/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_&%r6!rao1dl+&wkye8f9bu#mc7gr#^$bn!6^@_5oiurzgkw1j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['panopano-prod.herokuapp.com', 'panopano-stage.herokuapp.com', 'panopano.com.br']
 
 
 # Application definition
