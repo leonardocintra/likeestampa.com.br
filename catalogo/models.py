41cb5c36c40f33cb71e0d4de0d7b5b59ffd8a61b
leo <leonardo.ncintra@outlook.com>
2021-05-24 13:51:51 -0300

    preco default 35

--- catalogo/models.py
+++ catalogo/models.py
@@ -49,7 +49,7 @@ class Produto(models.Model):
     descricao = models.TextField('Descrição', blank=True)
     slug = models.SlugField('Identificador', max_length=100, unique=True)
     ativo = models.BooleanField(default=False)
-    preco_base = models.DecimalField('Preço base', decimal_places=2, max_digits=999, default=0)
+    preco_base = models.DecimalField('Preço base', decimal_places=2, max_digits=999, default=35.00)
     subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
     imagem_principal = CloudinaryField('Imagem principal', blank=True, null=True)
     created_at = models.DateField('Criado em', auto_now_add=True)
