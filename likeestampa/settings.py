0f0f89fc35925631253feb9fcedb0cd70b15f2a6
leo <leonardo.ncintra@outlook.com>
2021-05-24 13:25:05 -0300

    add urlib3

--- likeestampa/settings.py
+++ likeestampa/settings.py
@@ -128,8 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-DISABLE_COLLECTSTATIC=1
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
