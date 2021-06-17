dd2a16d9675bc34fef0b0aa9ea320be74de9565d
leo <leonardo.ncintra@outlook.com>
2021-06-16 23:03:22 -0300

    fix migration item cor tamanho

--- checkout/models.py
+++ checkout/models.py
@@ -20,8 +20,8 @@ class Carrinho(models.Model):
 class Item(models.Model):
     carrinho = models.ForeignKey(Carrinho, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    cor = models.ForeignKey(ProdutoVariacao, on_delete=models.CASCADE, default=1, related_name="item_cor")
-    tamanho = models.ForeignKey(ProdutoVariacao, on_delete=models.CASCADE, default=1, related_name="item_tamanho")
+    cor = models.ForeignKey(ProdutoVariacao, on_delete=models.SET_NULL, null=True, related_name="item_cor")
+    tamanho = models.ForeignKey(ProdutoVariacao, on_delete=models.SET_NULL, null=True, related_name="item_tamanho")
     quantidade = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Modificado em', auto_now=True)
