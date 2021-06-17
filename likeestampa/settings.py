c4ba8d863a138b68072bfde1caaac2be6199fad0
leo <leonardo.ncintra@outlook.com>
2021-06-17 20:43:15 -0300

    refatoracao template

--- likeestampa/settings.py
+++ likeestampa/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'likeestampa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
