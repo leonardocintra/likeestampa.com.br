a4fcd97b2211d6fdebfcd106af56252853d75d54
leo <leonardo.ncintra@outlook.com>
2021-06-19 14:33:37 -0300

    refatorando profile

--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from checkout.forms import ClienteForm
+from .business import get_cliente_data_form
 from .models import Cliente
 
 
@@ -7,7 +9,10 @@ from .models import Cliente
 def cliente(request):
     user = request.user
     cliente = Cliente.objects.get(user=user)
+    request.session['cliente_id'] = cliente.peoplesoft_id
+    form = get_cliente_data_form(request)
     context = {
-        'cliente': cliente
+        'cliente': cliente,
+        'form': form
     }
     return render(request, "usuario/profile.html", context)
