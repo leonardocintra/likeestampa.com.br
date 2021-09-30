ff57b103dd67d4feddf53bd8cc4cd12c4604fcc0
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2021-09-29 22:30:48 -0300

    add privacity

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import AboutView
+from .views import AboutView, TermosDeUsoView, TrocaCancelamentoView
 from catalogo import views
 
 app_name = 'core'
@@ -8,4 +8,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.product_list.as_view(), name='index'),
     path('quem-somos', AboutView.as_view(), name='about'),
+    path('troca-e-cancelamento', TrocaCancelamentoView.as_view(), name='trocacancelamento'),
+    path('termos-de-uso', TermosDeUsoView.as_view(), name='termos'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.views.generic.base import TemplateView
 
 class AboutView(TemplateView):
     template_name = "about.html"
+
+class TrocaCancelamentoView(TemplateView):
+    template_name = "troca-e-cancelamento.html"
+
+class TermosDeUsoView(TemplateView):
+    template_name = "termos-de-uso.html"
