2a2a1dcd39e0575dddb0b19c1b5d4ef42e4a009d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-02-01 17:57:26 -0300

    somente id do merchant order

--- pagamento/views.py
+++ pagamento/views.py
@@ -173,7 +173,7 @@ def mp_notifications(request):
 
         if request.GET.get('topic') == 'merchant_order':
             merchant_order = get_merchant_order(request.GET.get('id'))
-            enviar_mensagem(merchant_order, 'Merchant Order')
+            enviar_mensagem(merchant_order['id'], 'Merchant Order')
 
             if merchant_order['status'] == 404:
                 enviar_mensagem('IPN Merchant Order não encontrado: {0} - ID: {1}'.format(
