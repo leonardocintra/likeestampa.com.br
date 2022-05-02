096eef4df6795cebe69bbc66f539575a2bd886d2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-05-02 15:27:51 -0300

    fix: sonar sugestions #92

--- catalogo/templatetags/catalogo_extras.py
+++ catalogo/templatetags/catalogo_extras.py
@@ -5,8 +5,8 @@ from catalogo.models import TipoProduto
 register = template.Library()
 
 
-@register.simple_tag(name='imagemTipoProduto')
-def imagemTipoProduto(tipo_produto_id=1):
+@register.simple_tag(name='imagem_tipo_produto')
+def imagem_tipo_produto(tipo_produto_id=1):
     # para incluir a imagem aqui precisa adicionar manualmente na pasta core/static/img
     tipos_produto = TipoProduto.get_tipos_produto_ativo()
     imagem = tipos_produto.filter(id=tipo_produto_id).first()
