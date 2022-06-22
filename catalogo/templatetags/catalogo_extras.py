f97d362e57773fc4a6b5a5ad7cdb95e094e64222
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-06-22 19:27:42 -0300

    git add imagem #100

--- catalogo/templatetags/catalogo_extras.py
+++ catalogo/templatetags/catalogo_extras.py
@@ -10,4 +10,4 @@ def imagem_tipo_produto(tipo_produto_id=1):
     # para incluir a imagem aqui precisa adicionar manualmente na pasta core/static/img
     tipos_produto = TipoProduto.get_tipos_produto_ativo()
     imagem = tipos_produto.filter(id=tipo_produto_id).first()
-    return static('img/' + imagem.icone_fontawesome + '.gif')
+    return static('img/' + imagem.imagem + '.gif')
--- likeestampa/settings/base.py
+++ likeestampa/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'anymail',
+    'fontawesomefree',
 
     # LOCAL
     'core',
