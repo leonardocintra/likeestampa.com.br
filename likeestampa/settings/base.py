b494faee2853da198b54b4500d445c177d78df37
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-09-01 17:59:40 -0300

    static files root

--- likeestampa/settings/base.py
+++ likeestampa/settings/base.py
@@ -163,8 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
